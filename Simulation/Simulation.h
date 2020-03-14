a9f3e29bdf715e12b066bbc0e0fe5a3255d30e51
virgilius <virgilius.santos@acad.pucrs.br>
2020-03-14 07:48:11 -0300

    Initial Commit

---
+++ Simulation/Simulation.h
@@ -0,0 +1,19 @@
+//
+//  Simulation.h
+//  Simulation
+//
+//  Created by Virgilius Santos on 14/03/20.
+//  Copyright © 2020 Virgilius Santos. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Simulation.
+FOUNDATION_EXPORT double SimulationVersionNumber;
+
+//! Project version string for Simulation.
+FOUNDATION_EXPORT const unsigned char SimulationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Simulation/PublicHeader.h>
+
+
