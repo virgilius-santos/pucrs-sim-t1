e4f11aea703dec5c5823f6f826acca6f24ce57c1
virgilius <virgilius.santos@acad.pucrs.br>
2020-03-15 18:03:25 -0300

    update app

--- App/App/main.swift
+++ App/App/main.swift
@@ -2,6 +2,10 @@
 import Foundation
 import Simulation
 
+enum Simuladores {
+    case simples, encadeado
+}
+
 // para mudar as configurações do congruente,
 // altere os valores abaixo
 
@@ -10,36 +14,41 @@ import Simulation
 //        Config.CongruenteLinear.M = 9
 //        Config.CongruenteLinear.semente = 9
 
-FilaSimples(
-    taxaEntrada: Tempo(inicio: 1, fim: 2),
-    taxaSaida: Tempo(inicio: 2, fim: 4),
-    kendall: Kendall(
-        a: "G",
-        b: "G",
-        c: 1,
-        k: 10,
-        n: 10,
-        d: 10)
+let tipoSimulador = Simuladores.simples
+print("simulador escolhido: ", tipoSimulador)
+switch tipoSimulador {
+    case .simples:
+        FilaSimples(
+            taxaEntrada: Tempo(inicio: 1, fim: 2),
+            taxaSaida: Tempo(inicio: 2, fim: 4),
+            kendall: Kendall(
+                a: "G",
+                b: "G",
+                c: 1,
+                k: 10,
+                n: 10,
+                d: 10)
     )
-
-FilaEncadeada(
-    taxaEntradaFila1: Tempo(inicio: 1, fim: 2),
-    taxaSaidaFila1: Tempo(inicio: 2, fim: 4),
-    kendallFila1: Kendall(
-        a: "G",
-        b: "G",
-        c: 2,
-        k: 4,
-        n: 4,
-        d: 0),
-    taxaEntradaFila2: Tempo(inicio: 1, fim: 2),
-    taxaSaidaFila2: Tempo(inicio: 2, fim: 4),
-    kendallFila2: Kendall(
-        a: "G",
-        b: "G",
-        c: 2,
-        k: 4,
-        n: 0,
-        d: 0)
-)
-
+    
+    case .encadeado:
+        FilaEncadeada(
+            taxaEntradaFila1: Tempo(inicio: 1, fim: 2),
+            taxaSaidaFila1: Tempo(inicio: 2, fim: 4),
+            kendallFila1: Kendall(
+                a: "G",
+                b: "G",
+                c: 2,
+                k: 4,
+                n: 4,
+                d: 0),
+            taxaEntradaFila2: Tempo(inicio: 1, fim: 2),
+            taxaSaidaFila2: Tempo(inicio: 2, fim: 4),
+            kendallFila2: Kendall(
+                a: "G",
+                b: "G",
+                c: 2,
+                k: 4,
+                n: 0,
+                d: 0)
+    )
+}
